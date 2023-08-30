eee89aeeffd819fb319a4c6a5b7db8e6de3bd156
praveen <praveenryali058@gmail.com>
2023-08-30 12:14:44 +0530

    first commit

---
+++ Models/Comments.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Assignment_12.Models
+{
+    [Table("Comments")]
+    public class Comments
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime PublicationDate { get; set; }
+
+        public int PostId { get; set; }
+        public virtual Post Post { get; set; }
+    }
+}
